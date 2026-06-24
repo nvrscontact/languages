d70b38120799188d831bf2255ea08b53d4a530ff
nvrscontact <nvrscontact@gmail.com>
2026-06-24 18:08:10 +0200

    g

--- src/pages/private/metrics/RealLifeSituations/RealLifeSituations.jsx
+++ src/pages/private/metrics/RealLifeSituations/RealLifeSituations.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import findAll from '../../../../services/hooks/findAll';
+import { addRow } from '../../../../services/hooks/addRow';
 
 function RealLifeSituations() {
+
+  const situations = findAll("situations");
+
+  const [languageId, setLanguageId] = useState(0);
+  const [situation, setSituation] = useState("");
+  const [level, setLevel] = useState("");
+
+  function addSituation() {
+
+    const situationRow = {
+      language_id: languageId,
+      situation:situation,
+      level: level,
+      table_name: "situations"
+    };
+
+    addRow(situationRow);
+
+  }
+
   return (
-    <div>RealLifeSituations</div>
+    <>
+      <div className={`header-basic`}>
+        <h2>Real Life SItuations</h2>
+        <span>only those necessary to complete levels.</span>
+      </div>
+
+      {situations.map(s => (
+        <p key={s.id}>{s.situation} </p>
+      ))}
+
+      <input type="text" value={languageId}
+        onChange={e => setLanguageId(e.target.value)} />
+      <input type="text" value={situation}
+        onChange={e => setSituation(e.target.value)} />
+      <input type="text" value={level}
+        onChange={e => setLevel(e.target.value)} />
+
+      <button onClick={addSituation}>Agregame</button>
+    </>
   )
 }
 
